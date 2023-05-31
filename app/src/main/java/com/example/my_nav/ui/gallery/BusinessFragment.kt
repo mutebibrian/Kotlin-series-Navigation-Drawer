dda2a1972be321edc954b6ff1447e41f6ab6790e
BRIAN MUTEBI <33450898+mutebibrian@users.noreply.github.com>
2023-05-31 13:16:46 +0300

    Android Navigation

---
+++ app/src/main/java/com/example/my_nav/ui/gallery/BusinessFragment.kt
@@ -0,0 +1,19 @@
+package com.example.my_nav.ui.gallery
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.my_nav.R
+
+class BusinessFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.fragment_business,container,false)
+    }
+}
---
+++ app/src/main/java/com/example/my_nav/ui/home/HomeFragment.kt
@@ -0,0 +1,22 @@
+package com.example.my_nav.ui.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.my_nav.R
+import com.example.my_nav.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+      return inflater.inflate(R.layout.fragment_home,container,false)
+}
+}
